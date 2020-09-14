497be81c549cae08d2187387ca1d35494de732cc
fauzifadillah <fauzifadillah00@yahoo.com>
2020-09-14 15:17:56 +0700

    Strapi CHECK OK

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="container-home">
+  <div class="container-home" v-for="home in homes" :key="homes.id">
     <Navbar />
 
     <section class="row first-section">
       <div class="row align-items-center">
         <div class="col-md-12" data-aos="fade-right" data-aos-duration="2500">
-          <h1>Unity in Dispersity</h1>
-          <h3 data-aos="fade-up" data-aos-duration="2500">There is always something new</h3>
+          <h1>{{ home.Hero }}</h1>
+          <h3 data-aos="fade-up" data-aos-duration="2500">{{ home.Tagline}}</h3>
         </div>
       </div>
     </section>
@@ -99,13 +99,28 @@
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
 // import VueAos from 'vue-aos'
-
+import axios from "axios";
 export default {
   name: "Home",
   components: {
     Navbar,
     Footer,
     // VueAos
+  },
+  data() {
+    return {
+      homes: [],
+      error: null,
+    };
+  },
+  async mounted() {
+    try {
+      const response = await axios.get("http://188.166.246.154/homes");
+      this.homes = response.data;
+      console.log(response);
+    } catch (error) {
+      this.error = error;
+    }
   },
 };
 </script>
