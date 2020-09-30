bd117366feb76b1be29d1173c7c6a1322e4e6059
fauzifadillah <fauzifadillah00@yahoo.com>
2020-09-30 21:29:33 +0700

    30/09/20

--- src/components/ResearchesDetailContent/ResearchesDetailContent.vue
+++ src/components/ResearchesDetailContent/ResearchesDetailContent.vue
@@ -1,23 +1,25 @@
 <template>
   <div class="container-researchesdetailcontent">
-    <h1>{{content.title}}</h1>
-    <p class="pt-3">{{content.body}}</p>
-    <div class="row mx-0 mb-3">
+    <h1>{{ content.title }}</h1>
+    <div v-html="body" class="pt-3"></div>
+    <!-- <div class="row mx-0 mb-3">
       <img
         class="mx-auto researches-image"
         src="../../assets/explainersdetail.png"
       />
-    </div>
+    </div> -->
   </div>
 </template>
 
 <script>
+import marked from "marked";
 import axios from "axios";
 export default {
   name: "ResearchesDetailContent",
   data() {
     return {
       content: {},
+      body: {},
       contentId: 0,
     };
   },
@@ -27,19 +29,20 @@ export default {
     const contentId = this.$route.params.id;
 
     try {
-      const response = await axios
-        .get(`https://cms.finder.ac.id/research-explainers/${contentId}`)
+      const response = await axios.get(
+        `https://cms.finder.ac.id/research-explainers/${contentId}`
+      );
 
       console.log(response.data);
       this.content = response.data;
-
+      this.body = marked(this.content.body);
     } catch (error) {
       this.error = error;
     }
-  }
+  },
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 @import "ResearchesDetailContent.scss";
 </style>
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -8,7 +8,9 @@
           <h1>
             <span ref="hero" class="hidetext">{{ home.Hero }}</span>
           </h1>
-          <h3 ref="tagline" data-aos="fade-in" data-aos-duration="3000">{{ home.Tagline }}</h3>
+          <h3 ref="tagline" data-aos="fade-in" data-aos-duration="3000">
+            {{ home.Tagline }}
+          </h3>
         </div>
       </div>
     </section>
@@ -35,7 +37,9 @@
       <div class="row">
         <div class="col-md-6">
           <h2 class="pr-4">
-            <span ref="anchor1" class="hidetext">{{ home.Anchor_1_title }}</span>
+            <span ref="anchor1" class="hidetext">{{
+              home.Anchor_1_title
+            }}</span>
           </h2>
           <img src="../../assets/logo-aneh.png" />
         </div>
@@ -47,8 +51,15 @@
 
     <section class="fourth-section">
       <div class="row">
-        <div class="col-auto mr-md-auto p-0" data-aos="fade-right" data-aos-duration="2500">
-          <img class="img-section" v-bind:src="'https://cms.finder.ac.id' + home.Image_2[0].url" />
+        <div
+          class="col-auto mr-md-auto p-0"
+          data-aos="fade-right"
+          data-aos-duration="2500"
+        >
+          <img
+            class="img-section"
+            v-bind:src="'https://cms.finder.ac.id' + home.Image_2[0].url"
+          />
         </div>
         <div class="col-md-4"></div>
       </div>
@@ -56,12 +67,12 @@
 
     <section class="fifth-section">
       <div class="row">
-        <h2
-          class="col-12 p-0"
-          data-aos="fade-out"
-          data-aos-duration="2500"
-        >{{ home.Anchor_2_title }}</h2>
-        <h4 class="m-5" data-aos="fade-in" data-aos-duration="3000">{{ home.Anchor_2_desc }}</h4>
+        <h2 class="col-12 p-0" data-aos="fade-out" data-aos-duration="2500">
+          {{ home.Anchor_2_title }}
+        </h2>
+        <h4 class="m-5" data-aos="fade-in" data-aos-duration="3000">
+          {{ home.Anchor_2_desc }}
+        </h4>
         <div class="col-12 p-0">
           <img src="../../assets/logo-aneh.png" />
         </div>
@@ -71,20 +82,32 @@
     <section class="sixth-section">
       <div class="row">
         <div class="col-md-4"></div>
-        <div class="col-md-8 ml-md-auto p-0" data-aos="fade-up" data-aos-duration="1500">
-          <img class="img-section" v-bind:src="'https://cms.finder.ac.id' + home.Image_3[0].url" />
+        <div
+          class="col-md-8 ml-md-auto p-0"
+          data-aos="fade-up"
+          data-aos-duration="1500"
+        >
+          <img
+            class="img-section"
+            v-bind:src="'https://cms.finder.ac.id' + home.Image_3[0].url"
+          />
         </div>
       </div>
     </section>
 
     <section class="seventh-section">
       <div class="row">
-        <h2 data-aos="fade-in" data-aos-duration="3000">{{ home.Anchor_3_title }}</h2>
+        <h2 data-aos="fade-in" data-aos-duration="3000">
+          {{ home.Anchor_3_title }}
+        </h2>
       </div>
     </section>
 
     <section class="eighth-section">
-      <img class="img-section" v-bind:src="'https://cms.finder.ac.id' + home.Image_4[0].url" />
+      <img
+        class="img-section"
+        v-bind:src="'https://cms.finder.ac.id' + home.Image_4[0].url"
+      />
     </section>
 
     <Footer />
