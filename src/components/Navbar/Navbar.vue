1cba891319d5d553f65165451f0e40c6b7405c0f
fauzifadillah <fauzifadillah00@yahoo.com>
2020-10-30 00:29:36 +0700

    News redirection

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -32,7 +32,7 @@
                     <a href="/contact">Contact</a>
                   </li>
                   <li>
-                    <a href="/news">News/Events</a>
+                    <a href="https://www.blog.finder.ac.id">News/Events</a>
                   </li>
                 </ul>
               </div>
