895006f74638795bea67db65327d9dcb025d9dee
fauzifadillah <fauzifadillah00@yahoo.com>
2021-02-22 12:30:00 +0700

    Visitor Counter

--- src/views/Contact/Contact.vue
+++ src/views/Contact/Contact.vue
@@ -27,6 +27,7 @@
         </div>
       </div> -->
     </section>
+    <a style="margin-left: 7%; margin-top: 5%;" href="https://info.flagcounter.com/Fnz3"><img src="https://s05.flagcounter.com/count2/Fnz3/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_10/viewers_0/labels_0/pageviews_0/flags_0/percent_0/" alt="Flag Counter" border="0"></a>
     <!-- <section class="second-section">
       <img class="img-section" src="../../assets/image-1.png" />
     </section> -->
